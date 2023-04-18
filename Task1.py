2a4412f57c3a8542a3f8deaab36eb609603636b0
Dmitry Vasiliuk <usy222@mail.ru>
2023-04-18 20:54:39 +0300

    Цыклы Вывод в строку

--- Task1.py
+++ Task1.py
@@ -3,16 +3,16 @@
 # for i in range(0, 5, 2):
 #   print(i)
 
-# stroka = "qwerty"
-#   for el in stroka:
-#   print(el)
+stroka = "qwerty"
+for el in stroka:
+#  print(el)
+  print(el, end=" - ")  # условие вывада символов в строку
 
-
-i = 0
-while (i < 5):
-  n = int(input("Ввод: "))
-  if n == 0:
-    break
-  i+=1
-else:
-  print("Конец")
+# i = 0
+# while (i < 5):
+#   n = int(input("Ввод: "))
+#   if n == 0:
+#     break
+#   i+=1
+# else:
+#   print("Конец")
