f1e4b3f3e1049dc2263f87a06ca5d5500eb6871a
Dmitry Vasiliuk <usy222@mail.ru>
2023-04-18 20:47:37 +0300

    Цыклы

---
+++ Task1.py
@@ -0,0 +1,18 @@
+# ВИДЫ ЦИКЛОВ!!!!!
+
+# for i in range(0, 5, 2):
+#   print(i)
+
+# stroka = "qwerty"
+#   for el in stroka:
+#   print(el)
+
+
+i = 0
+while (i < 5):
+  n = int(input("Ввод: "))
+  if n == 0:
+    break
+  i+=1
+else:
+  print("Конец")
